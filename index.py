669aefc8ee739981d271bd4c98adae5fce2cd299
rohitganesh <thtgnsh5@gmail.com>
2022-02-01 20:03:40 +0530

    i added python classes

--- index.py
+++ index.py
@@ -1,3 +1,19 @@
-def add(x,y):
-    print(x+y)
-add(3,4)
+class parent:
+ def __init__(self,name,rollno) -> None:
+    self.name=name
+    self.rollno=rollno
+    print('From the constructor :',name,' ',rollno)
+ def details(self):
+     print('From the parent class details function :',self.name,' ',self.rollno)
+
+class child(parent):
+    def ganesh_details(self):
+        print('From ganesh team :',self.name,' ',self.rollno)
+
+obj=child('JAFFA',420)
+obj.ganesh_details()
+obj.details()
+
+
+
+
